98ce94ead92eb8a4eed6d1dc5e37cc571e326ee4
Md. Asadul Islam <asad.cse.ru.15@gmail.com>
2018-11-09 23:49:16 +0600

    Crypto library

--- config/secret.keys.js
+++ config/secret.keys.js
@@ -1,7 +1,7 @@
 var secretKeys = {
-	email: "^5F&hs",
-	userName: "Uf%f2",
-	password: "&hs#D6",
+	email: "^5F&hs^5F&hs^5F&hs^5F&hs^5F&hs23",  //should be 32 bytes
+	userName: "Uf%f2desUf%f2desUf%f2desUf%f2des",  //should be 32 bytes
+	password: "&hs#D63d&hs#D63d&hs#D63d&hs#D63d",  //should be 32 bytes
 	session: "Ggs5f",
 	cookie: "GFfss^",
 	jwt: "hsg^gsgJ"
